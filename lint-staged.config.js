a4ac53f29fa9c9d8cb1ebf7e7cfe215a9ecbbc11
GLD5000 <garethlouisdevlin@gmail.com>
2023-08-05 21:54:03 +0100

    Add linting and husky

---
+++ lint-staged.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // this will check Typescript files
+    '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
+
+    // This will format and lint TS & JS Files
+    '**/*.(ts|tsx|js)': (filenames) => [
+        `yarn prettier --write ${filenames.join(' ')}`,
+        `yarn eslint --fix ${filenames.join(' ')}`,
+    ],
+
+    // this will Format MarkDown and JSON
+    '**/*.(md|json)': (filenames) =>
+        `yarn prettier --write ${filenames.join(' ')}`,
+}
